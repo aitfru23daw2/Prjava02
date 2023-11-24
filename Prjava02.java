7bb8b5ce760ecdd658667a36e6e01db3feafe3b7
aitfru23daw2 <464425.clot@fje.edu>
2023-11-24 20:47:35 +0100

    Revert "Versió 1.3 del projecte prjava02" Error
    This reverts commit 64c12c7b9419f72e76bafa57d4ea2b3e8bc54e08.

--- Prjava02.java
+++ Prjava02.java
@@ -28,7 +28,6 @@ public class Prjava02 {
             System.out.println("Versió OS: " + System.getProperty("os.version"));
 	    System.out.println("Creació d'una branca del projecte prjava02");	    
        	    System.out.println("Afegint més codi des de la branca00 del projecte prjava02");
-	   System.out.println("Afegint codi des de la branca01 del projecte prjava02");
 
         }
         catch (IOException e) {
@@ -36,4 +35,3 @@ public class Prjava02 {
         }  
     }
 }
-
